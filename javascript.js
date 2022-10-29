85cb050fb23225a9fd866eafcf076010e89af4c8
Sebastian Romanczuk <sebastian.romanczuk1999@gmail.com>
2022-10-29 20:32:28 +0200

    Homepages

--- javascript.js
+++ javascript.js
@@ -24,17 +24,3 @@ function double(number) {
 
 let myNumber = 5;
 console.log(double(myNumber));
-
-console.log("HELLO KANTOR NIE BEMA")
-let formElement = document.querySelector(".js-F");
-let amountElement = document.querySelector(".js-A");
-let resultElement = document.querySelector(".js-R");
-let currencyElement = document.querySelector(".js-WC");
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let amount = amountElement.value;
-    let currency = currencyElement.value;
-    let result = (amount / currency);
-    resultElement.innerText = result.toFixed(2);
-});
-
